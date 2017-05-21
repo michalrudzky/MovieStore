7a1e8ed8d6dde1de0866103093041d8e76c44d34
USER\Michał <michal.rudzky@gmail.com>
2017-05-21 02:13:43 +0200

    Set up the DI container.

--- MovieStore.WebUI/App_Start/NinjectWebCommon.cs
+++ MovieStore.WebUI/App_Start/NinjectWebCommon.cs
@@ -61,6 +61,7 @@ namespace MovieStore.WebUI.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            System.Web.Mvc.DependencyResolver.SetResolver(new MovieStore.WebUI.Infrastructure.NinjectDependencyResolver(kernel));
         }        
     }
 }

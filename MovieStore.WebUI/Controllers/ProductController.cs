f707c3c2dae542b961a2909a33014b2ca97b4998
USER\Michał <michal.rudzky@gmail.com>
2017-05-21 02:26:56 +0200

    Created ProductController, added List action method.

---
+++ MovieStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieStore.Domain.Abstract;
+using MovieStore.Domain.Entities;
+
+namespace MovieStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}

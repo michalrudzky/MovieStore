5ad36ccc655a01d09da09e89fad84b0724b7f348
USER\Michał <michal.rudzky@gmail.com>
2017-05-22 18:28:36 +0200

    Added pagination.

--- MovieStore.WebUI/Controllers/ProductController.cs
+++ MovieStore.WebUI/Controllers/ProductController.cs
@@ -5,21 +5,34 @@ using System.Web;
 using System.Web.Mvc;
 using MovieStore.Domain.Abstract;
 using MovieStore.Domain.Entities;
+using MovieStore.WebUI.Models;
 
 namespace MovieStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            ProductsListViewModel model = new ProductsListViewModel {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
---
+++ MovieStore.WebUI/Models/ProductsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MovieStore.Domain.Entities;
+
+namespace MovieStore.WebUI.Models
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}

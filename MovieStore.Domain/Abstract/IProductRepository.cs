3c75b1eae80fd01a6c23b6fd540ef3650bd1cb32
USER\Michał <michal.rudzky@gmail.com>
2017-05-27 17:33:36 +0200

    Added admin features.

--- MovieStore.Domain/Abstract/IProductRepository.cs
+++ MovieStore.Domain/Abstract/IProductRepository.cs
@@ -6,5 +6,9 @@ namespace MovieStore.Domain.Abstract
     public interface IProductRepository
     {
         IEnumerable<Product> Products { get; }
+
+        void SaveProduct(Product product);
+
+        Product DeleteProduct(int productID);
     }
 }
--- MovieStore.Domain/Concrete/EFProductRepository.cs
+++ MovieStore.Domain/Concrete/EFProductRepository.cs
@@ -12,5 +12,36 @@ namespace MovieStore.Domain.Concrete
         {
             get { return context.Products; }
         }
+
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductID == 0)
+            {
+                context.Products.Add(product);
+            }
+            else
+            {
+                Product dbEntry = context.Products.Find(product.ProductID);
+                if (dbEntry != null)
+                {
+                    dbEntry.Name = product.Name;
+                    dbEntry.Description = product.Description;
+                    dbEntry.Price = product.Price;
+                    dbEntry.Category = product.Category;
+                }
+            }
+            context.SaveChanges();
+        }
+
+        public Product DeleteProduct(int productID)
+        {
+            Product dbEntry = context.Products.Find(productID);
+            if (dbEntry != null)
+            {
+                context.Products.Remove(dbEntry);
+                context.SaveChanges();
+            }
+            return dbEntry;
+        }
     }
 }
